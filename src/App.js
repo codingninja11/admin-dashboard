8a7ebf62a4cd6fddc1f952df128c84d6c834fd4f
codingninja11 <akritisheresth@gmail.com>
2021-08-13 18:10:10 +0530

    admin-dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-function App() {
+import { useState } from "react";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+
+const App = () => {
+  const [sidebarOpen, setsidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setsidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setsidebarOpen(false);
+  };
   return (
-    <div className="App">
-      hello
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <Main />
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import "./Navbar.css";
+import avatar from "../../assets/avatar.svg";
+
+const Navbar = ({ sidebarOpen, openSidebar }) => {
+  return (
+    <nav className="navbar">
+      <div className="nav_icon" onClick={() => openSidebar()}>
+        <i className="fa fa-bars" aria-hidden="true"></i>
+      </div>
+      <div className="navbar__left">
+       
+        <a className="active_link" href="#">
+          Admin
+        </a>
+      </div>
+      <div className="navbar__right">
+        <a href="#!">
+          <img width="30" src={avatar} alt="avatar" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
